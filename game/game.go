dc8f174545e7ea7543100b9ca2cae504ccd331f2
Manuel Dewald <manuel.dewald@codesphere.com>
2026-01-15 17:37:43 +0100

    flip by click

--- game/game.go
+++ game/game.go
@@ -11,11 +11,13 @@ import (
 )
 
 type Game struct {
-	Width   float32
-	Height  float32
-	Rules   []Rule
-	Card    Rule
-	History []Rule
+	Width        float32
+	Height       float32
+	Rules        []Rule
+	Card         Rule
+	SwapCard     bool
+	SwapDistance float32
+	History      []Rule
 }
 
 type Rule struct {
@@ -53,6 +55,11 @@ func (g *Game) Update() error {
 			g.History = g.History[:len(g.History)-1]
 		}
 	}
+
+	// next card when screen is tapped/clicked
+	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+		g.SelectCard()
+	}
 	return nil
 }
 
@@ -63,8 +70,21 @@ const (
 
 // Select random card
 func (g *Game) SelectCard() {
+	if g.SwapCard {
+		return
+	}
+
+	// Append current card to history
 	g.History = append(g.History, g.Card)
+
 	g.Card = g.Rules[rand.IntN(len(g.Rules))]
+	for g.Card.Title == g.History[len(g.History)-1].Title {
+		g.Card = g.Rules[rand.IntN(len(g.Rules))]
+	}
+	if len(g.History) > 1 {
+		g.SwapCard = true
+	}
+	g.SwapDistance = 0
 	// Only keep last 10 history items
 	if len(g.History) > 10 {
 		g.History = g.History[len(g.History)-10:]
@@ -72,39 +92,62 @@ func (g *Game) SelectCard() {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(color.RGBA{20, 35, 25, 255})
+
+	currentCard := ebiten.NewImage(int(g.Width), int(g.Height))
+
+	g.drawCard(currentCard, g.Card)
+
+	screen.DrawImage(currentCard, &ebiten.DrawImageOptions{})
+
+	if g.SwapCard {
+		flipAnimation := ebiten.DrawImageOptions{}
+		flipAnimation.GeoM.Translate(-float64(g.SwapDistance), 0)
+		oldCard := ebiten.NewImage(int(g.Width), int(g.Height))
+		g.drawCard(oldCard, g.History[len(g.History)-1])
+		screen.DrawImage(oldCard, &flipAnimation)
+		if g.SwapDistance < g.Width {
+			g.SwapDistance += 7
+		}
+		if g.SwapDistance >= g.Width {
+			g.SwapCard = false
+		}
+	}
+}
+
+func (g *Game) drawCard(image *ebiten.Image, card Rule) {
+	//image.Fill(color.RGBA{20, 35, 25, 255})
 
 	cardWidth := g.Width - 3
 	cardHeight := g.Height - 3
 
 	// Draw shadow
-	vector.FillRect(screen, 3, 3, cardWidth, cardHeight, color.RGBA{0, 0, 0, 150}, false)
+	vector.FillRect(image, 3, 3, cardWidth, cardHeight, color.RGBA{0, 0, 0, 20}, false)
 	// Use an "Off-White" or "Parchment" color
-	vector.FillRect(screen, 0, 0, cardWidth, cardHeight, color.RGBA{252, 245, 220, 255}, false)
+	vector.FillRect(image, 0, 0, cardWidth, cardHeight, color.RGBA{252, 245, 220, 255}, false)
 
 	// 5. Draw Card Border (Beveled look)
-	vector.StrokeRect(screen, 0, 0, cardWidth, cardHeight, 2, color.RGBA{40, 30, 20, 255}, false)
+	vector.StrokeRect(image, 0, 0, cardWidth, cardHeight, 2, color.RGBA{40, 30, 20, 20}, false)
 
 	// 6. Internal Content Layout
 	contentX := 10
 
 	textColor := color.RGBA{30, 20, 10, 255}
-	util.DrawWrappedText(screen, g.Card.Title, contentX, 10, int(cardWidth), 10, textColor)
+	util.DrawWrappedText(image, card.Title, contentX, 10, int(cardWidth), 10, textColor)
 
 	// Image Square (Centered inside card)
 	imgX := +(cardWidth - ImageWidth) / 2
 	imgY := float32(30)
-	vector.FillRect(screen, imgX, imgY, ImageWidth, ImageHeight, color.RGBA{220, 210, 190, 255}, false)
+	vector.FillRect(image, imgX, imgY, ImageWidth, ImageHeight, color.RGBA{220, 210, 190, 255}, false)
 
-	if g.Card.HasImage() {
+	if card.HasImage() {
 		imOp := &ebiten.DrawImageOptions{}
 		imOp.GeoM.Translate(float64(imgX), float64(imgY))
-		screen.DrawImage(g.Card.GetSprite(), imOp)
+		image.DrawImage(card.GetSprite(), imOp)
 	}
-	vector.StrokeRect(screen, imgX, imgY, ImageWidth, ImageHeight, 1, color.RGBA{100, 90, 80, 255}, false)
+	vector.StrokeRect(image, imgX, imgY, ImageWidth, ImageHeight, 1, color.RGBA{100, 90, 80, 255}, false)
 
 	ruleDescY := imgY + ImageHeight + 15
-	util.DrawWrappedText(screen, g.Card.Description, contentX, int(ruleDescY), int(cardWidth), 10, textColor)
+	util.DrawWrappedText(image, card.Description, contentX, int(ruleDescY), int(cardWidth), 10, textColor)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
